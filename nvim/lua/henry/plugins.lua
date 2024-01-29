7fb8a0967318dba5de17c7be8580a6cf85f364b3
Henry.Krueger <henry.krueger@newtec.de>
2024-01-29 15:44:43 +0100

    added language support package

--- nvim/lua/henry/plugins.lua
+++ nvim/lua/henry/plugins.lua
@@ -41,21 +41,37 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && npm install', cmd = 'MarkdownPreview'}
+	-- My plugins here
+	use "wbthomason/packer.nvim" -- Have packer manage itself
+	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+	use {'iamcco/markdown-preview.nvim', run = 'cd app && npm install', cmd = 'MarkdownPreview'}
   
-  -- ide styles
-  use "lunarvim/colorschemes" -- colorschehemes addon
-  use "lunarvim/darkplus.nvim"
-  use "folke/tokyonight.nvim"
+	-- ide styles
+	use "lunarvim/colorschemes" -- colorschehemes addon
+	use "lunarvim/darkplus.nvim"
+	use "folke/tokyonight.nvim"
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- cmp
+	use "hrsh7th/nvim-cmp" -- completion plug
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-path"
+	use "hrsh7th/cmp-cmdline"
+	use "saadparwaiz1/cmp_luasnip"
+	use "hrsh7th/cmp-nvim-lsp"
+
+	-- snippets
+	use "L3MON4D3/LuaSnip"
+	use "rafamadriz/friendly-snippets"
+
+	-- LSP
+	use "neovim/nvim-lspconfig" -- enable LSP
+	use "williamboman/nvim-lsp-installer" -- simple language server
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
 
